4cd75b17d8bafdb90d091b0c63bf3608ea8f9ded
Jeremiah Dulla <jeremiahdulla@gmail.com>
2024-08-14 00:33:27 -0400

    Add getter methods and refactor to use them

--- src/classes.js
+++ src/classes.js
@@ -5,10 +5,10 @@ class LinkedList {
 
   append(value) {
     const node = new Node(value);
-    if (this.head == null) {
+    if (this.getHead() == null) {
       this.head = node;
     } else {
-      let currentNode = this.head;
+      let currentNode = this.getHead();
       while (currentNode.next !== null) {
         currentNode = currentNode.next;
       }
@@ -18,13 +18,53 @@ class LinkedList {
 
   prepend(value) {
     const node = new Node(value);
-    if (this.head !== null) {
-      let prevHead = this.head;
+    if (this.getHead() !== null) {
+      let prevHead = this.getHead();
       this.head = node;
       node.next = prevHead;
     } else {
       this.head = node;
     }
+  }
+
+  getSize() {
+    let nodes = 0;
+    let currentNode = this.getHead();
+    if (currentNode == null) {
+      return nodes;
+    } else {
+      while (currentNode !== null) {
+        currentNode = currentNode.next;
+        nodes++;
+      }
+      return nodes;
+    }
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    let currentNode = this.getHead();
+    if (currentNode == null) {
+      return null;
+    } else {
+      while (currentNode.next !== null) currentNode = currentNode.next;
+      return currentNode;
+    }
+  }
+
+  getNodeAt(index) {
+    let currentNode = this.getHead();
+    if (index >= this.size() || index < 0) return null;
+
+    let node = 0;
+    while (node < index) {
+      currentNode = currentNode.next;
+      node++;
+    }
+    return currentNode;
   }
 }
 
