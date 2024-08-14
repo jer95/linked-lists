34bba518842733923f18fcded74b1e8cda3751d9
Jeremiah Dulla <jeremiahdulla@gmail.com>
2024-08-13 23:38:49 -0400

    Add classes

---
+++ src/classes.js
@@ -0,0 +1,12 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+class Node {
+  constructor(value = null, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
